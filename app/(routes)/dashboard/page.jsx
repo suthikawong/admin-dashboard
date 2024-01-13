21443c8b6d33b506a31f7298fa92bcae8c46f73c
suthikawong <suthika.wsrd@gmail.com>
2024-01-13 19:02:18 +0700

    create card & transactions

--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -1,5 +1,25 @@
 import React from 'react'
+import styles from '../../ui/dashboard/dashboard.module.css'
+import Card from '@/app/ui/dashboard/card/card'
+import Rightbar from '@/app/ui/dashboard/rightbar/rightbar'
+import Transactions from '@/app/ui/dashboard/transactions/transactions'
+import Chart from '@/app/ui/dashboard/chart/chart'
 
 export default function Dashboard() {
-  return <div>Dashboard</div>
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Transactions />
+        <Chart />
+      </div>
+      <div className={styles.side}>
+        <Rightbar />
+      </div>
+    </div>
+  )
 }
---
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './rightbar.module.css'
+
+function Rightbar() {
+  return <div className={styles.container}>Rightbar</div>
+}
+
+export default Rightbar
