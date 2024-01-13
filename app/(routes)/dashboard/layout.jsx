6995175fb78f1e38586aef05a627c6eae6594630
suthikawong <suthika.wsrd@gmail.com>
2024-01-13 23:05:04 +0700

    create rightbar & footer

--- app/(routes)/dashboard/layout.jsx
+++ app/(routes)/dashboard/layout.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '@/app/ui/dashboard/navbar/navbar'
 import Sidebar from '@/app/ui/dashboard/sidebar/sidebar'
 import styles from '../../ui/dashboard/dashboard.module.css'
+import Footer from '@/app/ui/dashboard/footer/footer'
 
 function Layout({ children }) {
   return (
@@ -12,6 +13,7 @@ function Layout({ children }) {
       <div className={styles.content}>
         <Navbar />
         {children}
+        <Footer />
       </div>
     </div>
   )
